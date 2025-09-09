ee603a8c0b66834c993f78477339706e5edb0c4f
Nguyen Minh Tam <ngmitamit@gmail.com>
2025-09-09 11:20:15 +0700

    Implement buff effect application and removal in MyGameplayAbility
    - Added ActivateAbility and EndAbility overrides to apply and remove buff effects using GAS
    - Updated MyGameplayAbility.h with necessary includes, properties, and method declarations
    - Modified related animation assets for character jump animations
    - Added new in-place jump left animation asset

--- Source/ActionRPG/Private/MyGameplayAbility.cpp
+++ Source/ActionRPG/Private/MyGameplayAbility.cpp
@@ -1,3 +1,38 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "MyGameplayAbility.h"
+#include "AbilitySystemComponent.h"
+
+// Apply a buff effect when the ability is activated
+void UMyGameplayAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo *ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData *TriggerEventData)
+{
+    Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+    if (BuffEffect && ActorInfo->OwnerActor.IsValid())
+    {
+        FGameplayEffectContextHandle EffectContext = ActorInfo->AbilitySystemComponent->MakeEffectContext();
+        EffectContext.AddSourceObject(this);
+
+        FGameplayEffectSpecHandle SpecHandle = MakeOutgoingGameplayEffectSpec(BuffEffect, GetAbilityLevel(Handle, ActorInfo));
+        if (SpecHandle.IsValid())
+        {
+            BuffHandle = ApplyGameplayEffectSpecToOwner(Handle, ActorInfo, ActivationInfo, SpecHandle);
+        }
+    }
+}
+
+// Remove the buff effect when the ability ends
+void UMyGameplayAbility::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo *ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+{
+    Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+
+    if (BuffHandle.IsValid() && ActorInfo->OwnerActor.IsValid())
+    {
+        UAbilitySystemComponent *AbilitySystem = ActorInfo->AbilitySystemComponent.Get();
+        if (AbilitySystem)
+        {
+            AbilitySystem->RemoveActiveGameplayEffect(BuffHandle);
+            BuffHandle.Invalidate();
+        }
+    }
+}
--- Source/ActionRPG/Public/MyGameplayAbility.h
+++ Source/ActionRPG/Public/MyGameplayAbility.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "Abilities/GameplayAbility.h"
 #include "MyAbilityTypes.h"
+#include "GameplayEffectTypes.h"
 #include "MyGameplayAbility.generated.h"
 
 UCLASS()
@@ -16,4 +17,14 @@ public:
 	// Ability Input ID
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Ability")
 	EMyAbilityInputID AbilityInputID = EMyAbilityInputID::None;
+
+	// Buff Effect
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Buff")
+	TSubclassOf<class UGameplayEffect> BuffEffect;
+
+	void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo *ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData *TriggerEventData);
+	void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo *ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled);
+
+private:
+	FActiveGameplayEffectHandle BuffHandle;
 };
